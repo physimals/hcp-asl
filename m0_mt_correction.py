1c94a5c3f3e4c1b595ba59346005f5e5089a97de
jackt3 <38471682+jackt3@users.noreply.github.com>
2020-04-20 12:42:06 +0100

    create load_json function
    Put json loading and error-checking into a function as this will be needed in later steps of the pipeline too.

--- m0_mt_correction.py
+++ m0_mt_correction.py
@@ -8,6 +8,20 @@ from pathlib import Path
 from fsl.wrappers import fslmaths, LOAD, bet, fast
 from initial_bookkeeping import create_dirs
 import subprocess
+
+def load_json(json_name):
+    """
+    Load json but with some error-checking to make sure it exists.
+    If it doesn't exist, instruct user to run the first part of 
+    the pipeline first.
+    """
+    if json_name.exists():
+        with open(json_name, 'r') as infile:
+            json_dict = json.load(infile)
+    else:
+        raise Exception(f'File {json_name} does not exist.' + 
+                        'Please run initial_processing() first.')
+
 def correct_M0(subject_dir, mt_factors):
     """
     Correct the M0 images for a particular subject whose data 
@@ -27,12 +41,7 @@ def correct_M0(subject_dir, mt_factors):
         # if doesn't exist, throw error and ask user to run
         # initial_processing() from initial_bookkeeping.py first
     json_name = subject_dir / 'ASL/ASL.json'
-    if json_name.exists():
-        with open(json_name, 'r') as infile:
-            json_dict = json.load(infile)
-    else:
-        raise Exception(f'File {json_name} does not exist.' + 
-                        'Please run initial_processing() first.')
+    load_json(json_name)
     
     # do for both m0 images for the subject, calib0 and calib1
     calib_names = [json_dict['CALIB0_img'], json_dict['CALIB1_img']]
