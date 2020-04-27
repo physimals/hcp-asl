537c3f15c54c464315ff9aae7b0178dded916df5
IBME user <flora.kennedy-mcconnell@eng.ox.ac.uk>
2020-04-27 15:48:45 +0100

    Completed analysis step functions ressst still work in progress

--- distcorr_warps.py
+++ distcorr_warps.py
@@ -1,9 +1,13 @@
-# Script for gradient and EPI ddistortion correction of HCP ASL data
+""" 
+ Script for gradient and EPI ddistortion correction of HCP ASL data
 
-### Script to generate gradient distortion correction, and EPI distortion
-### correction warps for the HCP ASL data, then apply the warps along 
-### with motion correction to the ASL-gridded T1w-space, so that all 
-### transformations are done in a single interpolation step.
+ Script to generate gradient distortion correction, and EPI distortion
+ correction warps for the HCP ASL data, then apply the warps along 
+ with motion correction to the ASL-gridded T1w-space, so that all 
+ transformations are done in a single interpolation step.
+
+ F. A. Kennedy McConnell - April 2020
+"""
 
 import nibabel as nb
 import numpy as np
@@ -11,79 +15,103 @@ import os
 import subprocess as sp
 
 # Generate gradient distortion correction warp
-def calc_gdc_warp(asldata_vol1, coeffs_loc):
-    ##### TO DO #####
-    # Got to be careful with the command below - gradient_unwarp.py 
-    # will output files directly into the current working directory...
+def calc_gdc_warp(asldata_vol1, coeffs_loc, oph):
+    """
+    Generate warp for gradient distortion correction using siemens 
+    coefficients file.
+    """
+
     gdc_call = ("python gradient_unwarp.py " + asldata_vol1 + " gdc_corr_vol1.nii.gz " +
                     "siemens -g " + coeffs_loc)
 
     print(gdc_call)
     #sp.run(gdc_call.split(), check=True, stderr=sp.PIPE, stdout=sp.PIPE)
 
-    gdc_warp_call = ("convertwarp --abs --ref=gdc_corr_vol1.nii.gz " +
-                    "--warp1=fullWarp_abs.nii.gz --relout --out=gdc_warp_path")
+    gdc_warp_call = ("convertwarp --abs --ref=" + oph + "/gdc_corr_vol1.nii.gz " +
+                    "--warp1" + oph + "/fullWarp_abs.nii.gz --relout --out=" + 
+                    oph + "/gdc_warp.nii.gz")
 
     print(gdc_warp_call)
     #sp.run(gdc_warp_call.split(), check=True, stderr=sp.PIPE, stdout=sp.PIPE)
 
 def produce_topup_params(pars_filepath):
+    """
+    Generate a file containing the parameters used by topup to generate EPI distortion
+    correction fieldmap and output this into subject's directory. -- This could be a 
+    global/config file if that is preferable
+    """
     with open(pars_filepath, "a") as t_pars:
         t_pars.write("0 1 0 0.04845" + "\n")
         t_pars.write("0 -1 0 0.04845" + "\n")
     
-def calc_fmaps(pa_ap_sefms, pars_filepath, cnf_file, out_basename, out_fmap, out_corrected_sefms, fmapmag, fmapmagbrain):
+def calc_fmaps(pa_ap_sefms, pars_filepath, cnf_file, distcorr_dir, out_basename, topup_fmap, 
+                fmap_rads, fmapmag, fmapmagbrain):
+
+    """
+    Use topup to generate a fieldmap for distortion correction in asl_reg 
+    after conversion from Hz to rads/s. Topup also outputs the SEFMs 
+    corrected for EPI distortion, which are then available for use as the 
+    fieldmap magnitude image and  brain-extracted fieldmap mag image.
+    """
     
-    topup_call = ("topup --imain=" + pa_ap_sefms + " --datain=" + pars_filepath +
-                    " --config=" + cnf_file + " --out=" + out_basename +
-                    " --fout=" + out_fmap + " --iout=out_corrected_sefms")
+    topup_call = ("topup --imain=" + distcorr_dir + "/" + pa_ap_sefms + 
+                    " --datain=" + pars_filepath + " --config=" + cnf_file + 
+                    " --out=" + distcorr_dir + "/" + out_basename + " --fout=" +
+                    distcorr_dir + "/" + out_fmap + " --iout=" + distcorr_dir +
+                    "corrected_sefms.nii.gz")
     
     print(topup_call)
     # sp.run(topup_call.split(), check=True, stderr=PIPE, stdout=sp.PIPE)
 
-    convert_torads = ("fslmaths " + out_fmap + " -mul 3.14159 -mul 2 " 
-                        + fmap_rads)
+    convert_torads = ("fslmaths " + distcorr_dir + "/" + out_fmap + 
+                        " -mul 3.14159 -mul 2 " + distcorr_dir + "/" + fmap_rads)
     print(convert_torads)
     # sp.run(convert_torads.split(), check=True, stderr=PIPE, stdout=PIPE)
 
-    mean_fmapmag_call = ("fslmaths " + out_corrected_sefms + " -Tmean " + 
-                            fmapmag)
+    mean_fmapmag_call = ("fslmaths " + distcorr_dir + "/corrected_sefms.nii.gz -Tmean " + 
+                            distcorr_dir + "/" + fmapmag)
     print(mean_fmapmag_call)
     # sp.run(mean_fmapmag_call.split(), check=True, stderr=PIPE, stdout=PIPE)
 
-    bet_fmapmag_call = ("bet " + fmapmag + " " + fmapmagbrain)
+    bet_fmapmag_call = ("bet " + distcorr_dir + "/" + fmapmag + " " + distcorr_dir +
+                            "/" + fmapmagbrain)
     print(bet_fmapmag_call)
     # sp.run(bet_fmapmag_call.split(), check=True, stderr=PIPE, stdout=PIPE)
 
-def calc_distcorr_warp(regfrom, distcorr_dir, ):
-    print("Performing distortion correction using asl_reg")
+def calc_distcorr_warp(regfrom, distcorr_dir, struct, struct_brain, mask, tissseg,
+                        asl2struct_trans, fmap_rads, fmapmag, fmapmagbrain, asl_grid_T1,
+                        gdc_warp):
+    """
+    Warp is generated from asl_reg, which performs EPI distortion correction of 
+    ASL data while transforming the data to alignment with T1w-space. This warp is
+    then merged with the gradient distortion correction warp if that is available.
+    """
 
     ## Use asl_reg to inital EPI distcorr warp
     reg_call = ("asl_reg -i " + regfrom + " -o " + distcorr_dir + " -s " +
                 struct + " --sbet=" + struct_brain + " -m " + mask + " --tissseg " +
-                tissseg + " --imat " + asl2struct_trans " --finalonly --fmap=" + 
+                tissseg + " --imat " + asl2struct_trans + " --finalonly --fmap=" + 
                 fmap_rads + " --fmapmag=" + fmapmag + " --fmapmagbrain=" + fmapmagbrain +
                 " --pedir=y --echospacing=0.00057")
     print(reg_call)
     # sp.run(reg_call.split(), check=True, stderr=PIPE, stdout=PIPE)
     
-    # get the warp into ASL space
-    convertwarp_call = ("convertwarp -r " + regfrom + " -o " + distcorr_dir + "/epi_dist_warp" + 
-                        " -w " + distcorr_dir +   "/asl2struct_warp.nii.gz --postmat=" + 
-                        struct2asl_trans + " --rel")
-    print(convertwarp_call)
-    # sp.run(convertwarp_call.split(), check=True, stderr=PIPE, stdout=PIPE)
-
     # Add the gradient distortion correction warp to the EPI distortion correction warp
-    ##### Need to make this conditional on the existence of the GDC warp
-    merge_warp_call = ("convertwarp -r " + regfrom + " -o " + distcorr_dir + 
-                        "/distcorr_warp -w " + distcorr_dir + "/epi_dist_warp --warp2=" + 
+    if os.path.isfile(gdc_warp):
+        merge_warp_call = ("convertwarp -r " + asl_grid_T1 + " -o " + distcorr_dir + 
+                        "/distcorr_warp -w " + distcorr_dir + "/asl2struct_warp --warp2=" + 
                         gdc_warp + " --rel")
-    print(merge_warp_call)
-    # sp.run(merge_warp_call.split(), check=True, stderr=PIPE, stdout=PIPE)
+        print(merge_warp_call)
+        # sp.run(merge_warp_call.split(), check=True, stderr=PIPE, stdout=PIPE)
+    else:
+        print("Gradtient distortion correction not applied")
 
 # calculate the jacobian of the warp for intensity correction
-def calc_warp_jacobian():
+def calc_warp_jacobian(distcorr_dir):
+    """
+    Calculation of the Jacobian of the combined distortion correction for subsequent 
+    Jacobian intensity scaling
+    """
     utils_call1 = ("fnirtfileutils -i " + distcorr_dir + "/distcorr_warp -f spline -o " + 
                     distcorr_dir + "/distcorr_warp_coeff")
     utils_call2 = ("fnirtfileutils -i " + distcorr_dir + "/distcorr_warp_coeff -j " +
@@ -100,22 +128,48 @@ def calc_warp_jacobian():
     # sp.run(hdr_call.split(), check=True, stderr=PIPE, stdout=PIPE)
 
 # apply the combined distortion correction warp
-def apply_distcorr_warp(distcorr_dir, motion_transforms):
+def apply_distcorr_warp(asldata_orig, T1space_ref, asldata_T1space, distcorr_dir,
+                        moco_xfms, calib_orig, calib_T1space, calib_xfms):
+    """
+    Application of motion correction, and EPI and gradient distortion corrections, 
+    and transformation to T1w-space in one step. Leaving the ASL and calibration data 
+    in ASL-gridded T1w-space.
+    """
 
-    apply_call = ("applywarp -i " + asldata_orig " -r " + T1space_ref + " -o " +
+    asl_apply_call = ("applywarp -i " + asldata_orig + " -r " + T1space_ref + " -o " +
                     asldata_T1space + " --premat=" + moco_xfms + " -w " + 
                     distcorr_dir + "/distcorr_warp" + " --rel --interp=trilinear" +
                     " --paddingsize=1 --super --superlevel=a")
 
-    ### Not sure how to do the ASL data Jacobian intensity scaling given that 
-    ### the data is being transformed into ASL-gridded T1w space - what are the 
-    ### properties of the Jacobian? The warp will be resolution independent, but
-    ### obviously isn't space-independent.
+    asl_jaco_call = ("fslmaths " + asldata_T1space + " -mul " + distcorr_dir + 
+                    "/distcorr_jacobian " + asldata_T1space)
 
-    # If I were trying to stay in ASL space then the bash command could just be:
-    # fslmaths $tempdir/asldata -mul $jacobian $tempdir/asldata
-    # then it would redo the averaging of the asl data to reproduce regfrom - 
-    # which wouldnt't be needed here...
+    calib_apply_call = ("applywarp -i " + calib_orig + " -r " + T1space_ref + " -o " +
+                    calib_T1space + " --premat=" + calib_xfms + " -w " + 
+                    distcorr_dir + "/distcorr_warp" + " --rel --interp=trilinear" +
+                    " --paddingsize=1 --super --superlevel=a")
+
+    calib_jaco_call = ("fslmaths " + calib_T1space + " -mul " + distcorr_dir + 
+                    "/distcorr_jacobian " + calib_T1space)
+    print(asl_apply_call)
+    print(asl_jaco_call)
+    print(calib_apply_call)
+    print(calib_jaco_call)
+
+    # sp.run(asl_apply_call.split(), check=True, stderr=PIPE, stdout=PIPE)
+    # sp.run(asl_jaco_call.split(), check=True, stderr=PIPE, stdout=PIPE)
+    # sp.run(calib_apply_call.split(), check=True, stderr=PIPE, stdout=PIPE)
+    # sp.run(calib_jaco_call.split(), check=True, stderr=PIPE, stdout=PIPE)
 
 if __name__ == "__main__":
     # fill in function calls
+
+    temp_dir = something
+    changedir = ("cd " + temp_dir)
+    sp.run(changedir.split())
+
+    # Call function to generate GDC warp:
+    asl1 = 
+    grad_coeffs = 
+    oph 
+
