a1ea443e20bd79464b9798e425548cb34e4a39c5
jackt3 <38471682+jackt3@users.noreply.github.com>
2020-04-30 15:30:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='hcp-asl', # is hyphen ok here and below or should they all be underscores?
+    name='hcpasl', # is hyphen ok here and below or should they all be underscores?
     entry_points={
-        'console_scripts': ['hcp_asl = hcp-asl.scripts.pipeline:main']
+        'console_scripts': ['hcp_asl = hcpasl.scripts.run_pipeline:main']
     }
     python_requires=['>=3.4'] # for pathlib.Path
     setup_requires=[
