01b631e01184fc2994fe716b948d896fd9e0a0e9
jackt3 <jackatoner@gmail.com>
2020-11-04 23:11:52 +0000

    Use lta_convert
    bbregister's --fslout was occasionally giving an fsl matrix with a final row != [0 0 0 1].

--- scripts/distcorr_warps.py
+++ scripts/distcorr_warps.py
@@ -51,6 +51,11 @@ def generate_asl2struct_initial(asl_vol0, struct, fsdir, reg_dir):
     cmd = f"$FREESURFER_HOME/bin/bbregister --s {sid} --mov {asl_vol0} --t1 "
     cmd += f"--reg asl2orig_mgz_initial_bbr.dat --fslmat {omat_path}"
     sp.run(cmd, shell=True)
+
+    # lta_convert
+    cmd = f"lta_convert --inreg asl2orig_mgz_initial_bbr.dat --outfsl {omat_path} "
+    cmd += f"--src {asl_vol0} --trg {orig_mgz}"
+    sp.run(cmd, shell=True)
     asl2orig_fsl = rt.Registration.from_flirt(omat_path, asl_vol0, orig_mgz)
 
     # Return to original working directory, and flip the FSL matrix to target
