92bb81a27adf35368cf5fe7d05aad8a297bda49c
jackt3 <jackatoner@gmail.com>
2020-11-05 17:26:30 +0000

    Import subjects list as .txt file

--- MTEstimation/mt_estimation_pipeline.py
+++ MTEstimation/mt_estimation_pipeline.py
@@ -6,110 +6,47 @@ import multiprocessing as mp
 from functools import partial
 
 TR = 8
-hcp_dir = Path().home() / 'Documents/Data/HCP_data/Aging'
-subjects = [
-    'HCA7025253',
-    'HCA6062456',
-    'HCA6731574',
-    'HCA6947294',
-    'HCA6785193',
-    'HCA6706373',
-    'HCA6782995',
-    'HCA6358881',
-    'HCA6068670',
-    'HCA6058970',
-    'HCA6635679',
-    'HCA7155973',
-    'HCA6498190',
-    'HCA6820674',
-    'HCA6603969',
-    'HCA7101546',
-    'HCA6946191',
-    'HCA7124659',
-    'HCA6949399',
-    'HCA6047359',
-    'HCA6757390',
-    'HCA7103651',
-    'HCA6475986',
-    'HCA6595794',
-    'HCA6197580',
-    'HCA6176471',
-    'HCA6788199',
-    'HCA7216462',
-    'HCA6618679',
-    'HCA6968202',
-    'HCA6678495',
-    'HCA7078981',
-    'HCA6393176',
-    'HCA7095779',
-    'HCA6504866',
-    'HCA6347674',
-    'HCA6470673',
-    'HCA7000843',
-    'HCA6430257',
-    'HCA6234964',
-    'HCA7222154',
-    'HCA6686191',
-    'HCA6542470',
-    'HCA7178177',
-    'HCA7030751',
-    'HCA6924080',
-    'HCA7121350'
-]
-subject_dirs = []
-for subject in subjects:
-    subject_dirs.append(hcp_dir / subject)
-
-rois = ['wm', 'gm', 'csf', 'combined']
-
-setup_call = partial(setup_mtestimation, rois=rois)
-with multiprocessing.Pool(multiprocessing.cpu_count()-2) as pool:
-    results = pool.map(setup_call, subject_dirs)
-for result in results:
-    print(result)
-estimate_mt(subject_dirs, rois, TR, 'together')
+ROIS = {
+    "wm": ["wm"],
+    "gm": ["gm"],
+    "csf": ["csf"],
+    "combined": ["combined"],
+    "all": ["wm", "gm", "csf", "combined"]
+}
 
 def main():
     # argument handling
     parser = argparse.ArgumentParser(
         description="Run the MT estimation pipeline."
     )
-    parser.add_argument(
-        "--subjectlist",
+    parser.add_argument("--subjectlist",
         help="A .txt file of subject names from whom we "
             +"wish to estimate the MT scaling factors.",
         required=True
     )
-    parser.add_argument(
-        "--studydir",
+    parser.add_argument("--studydir",
         help="Path to the study's base directory.",
         required=True
     )
-    parser.add_argument(
-        "--method",
+    parser.add_argument("--method",
         help="Method of bias correction to use. Default is "
             +"'calib'.",
         default='calib',
         choices=('calib', 't1', 'sebased')
     )
-    parser.add_argument(
-        "-c",
-        "--cores",
+    parser.add_argument("-c", "--cores",
         help="Number of cores to use. Default is the number "
             +f"of cores your machine has ({mp.cpu_count()}).",
         default=mp.cpu_count(),
         type=int,
         choices=range(1, mp.cpu_count()+1)
     )
-    parser.add_argument(
-        "--roi",
+    parser.add_argument("--roi",
         help="Tissue in which to estimate the MT scaling factors.",
         default="combined",
-        choices=("combined", "wm", "gm", "csf")
+        choices=("combined", "wm", "gm", "csf", "all")
     )
-    parser.add_argument(
-        "-v",
-        "--verbose",
+    parser.add_argument("-v", "--verbose",
         help="Print some useful statements.",
         action="store_true"
     )
@@ -117,9 +54,20 @@ def main():
     # parse
     studydir = Path(args.studydir).resolve(strict=True)
     subjects = Path(args.subjects).resolve(strict=True)
-    subjects = np.loadtxt(subjects)
+    subjects = np.loadtxt(subjects, dtype=str)
     subjects = [studydir / subid for subid in subjects]
+    rois = ROIS[args.roi]
     if args.verbose:
         print(f"Your study directory is {studydir}.")
         print(f"You are processing {len(subjects)} subjects.")
-    
+        print(f"Rois to be used for the estimation: {rois}.")
+
+    # do setup
+    setup_call = partial(setup_mtestimation, rois)
+    with mp.Pool(args.cores) as pool:
+        results = pool.map(setup_call, subjects)
+    for result in results:
+        print(result)
+
+    # do estimation
+    estimate_mt(subject_dirs, rois, TR, 'separate')
