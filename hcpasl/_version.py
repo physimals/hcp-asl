5d4ef3e5f0e4ca4a282f58306295655f723ebc43
Martin Craig <martin.craig@eng.ox.ac.uk>
2021-02-03 16:33:12 +0000

    Remove version file which is autogenerated and not part of git

--- hcpasl/_version.py
+++ hcpasl/_version.py
@@ -1,2 +1,3 @@
-__version__ = '0.0.1.post15'
-__timestamp__ = 'Thu Dec 3 17:27:01 2020 +0000'
+__version__ = '0.0.7'
+__timestamp__ = 'Tue Feb 2 16:34:37 2021 +0000'
+__sha1__ = 'fdb96efc75e6b48984932ffc561630d19c32830f'
