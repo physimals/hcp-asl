d71389726708027b53f927650dab5d82720d7e8f
Tom Kirk <tomfrankkirk@gmail.com>
2023-08-03 12:47:24 +0100

    Change QC report location

--- hcpasl/qc.py
+++ hcpasl/qc.py
@@ -19,11 +19,9 @@ def create_qc_report(subject_dir, outdir):
         outdir = subject_dir / outdir
 
     # Sneaky redirection of paths.
-    # HCPASL outputs need to be referenced to the output directory
-    # The scene file is inserted into the subjects MNINonLinear preprocessed directory
-    # (not necessarily the same as the output) so it can pick up all the surfaces
-    # etc automatically. Then the pipeline outputs are templated in using
-    # relative paths. Finally we re-base the whole scene using wb_command.
+    # The template scene file is referenced to MNINonLinear/ASL/ASLQC
+    # So we generate the scene file there, template in subject-specific data, 
+    # and then re-base the scene to T1w/ASL/ASLQC
     subject_id = subject_dir.stem
     scene_initial = Path(subject_dir) / f"MNINonLinear/ASL/ASLQC/{subject_id}_hcp_asl_qc.scene"
     rel_path_outdir = os.path.relpath(outdir / "T1w/ASL", start=scene_initial.parent)
@@ -42,15 +40,13 @@ def create_qc_report(subject_dir, outdir):
 
     # If an outdir has been set, rebase scene to outdir/MNINonLinear/ASL/ASLQC
     # then remove the initial one 
-    scene_final = outdir / f"MNINonLinear/ASL/ASLQC/{subject_id}_hcp_asl_qc.scene"
-    if scene_final != scene_initial: 
-        scene_final.parent.mkdir(parents=True, exist_ok=True)
-        logging.info(f"Generating wb_view QC scene at {scene_final}")
-        cmd = ["wb_command", "-scene-file-relocate", str(scene_initial), str(scene_final)]
-        subprocess_popen(cmd)
-        os.remove(scene_initial)
-    else: 
-        scene_final = scene_initial
+    scene_final = outdir / f"T1w/ASL/ASLQC/{subject_id}_hcp_asl_qc.scene"
+    scene_final.parent.mkdir(parents=True, exist_ok=True)
+    logging.info(f"Generating wb_view QC scene at {scene_final}")
+    cmd = ["wb_command", "-scene-file-relocate", str(scene_initial), str(scene_final)]
+    subprocess_popen(cmd)
+    os.remove(scene_initial)
+    os.rmdir(scene_initial.parent)
 
     wb_cmd = os.environ["CARET7DIR"] + "/wb_command"
     for idx in range(1, 8):
@@ -84,7 +80,7 @@ def create_qc_report(subject_dir, outdir):
     new_nb_name = (
         Path(subject_dir)
         / outdir
-        / f"MNINonLinear/ASL/ASLQC/{subject_id}_hcp_asl_report.ipynb"
+        / f"T1w/ASL/ASLQC/{subject_id}_hcp_asl_report.ipynb"
     )
     logging.info(f"Generating jupyter notebook at {new_nb_name}")
     with open(new_nb_name, "w") as f:
